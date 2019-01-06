e3e355f64f3528cb0775341f0f9783f4eee6ac00
Ethan Crawford <ethcrawford@gmail.com>
2019-01-06 12:00:47 +0300

    Add Modes

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,29 @@
 
 const { resolve } = require("path");
 
-module.exports = {
-  mode: "production",
-  entry: "./src/index",
-  output: {
-    path: resolve(__dirname, "build"),
-    filename: "static/js/[name].[chunkhash:8].js",
-    chunkFilename: "static/js/[name].[chunkhash:8].chunk.js",
-    publicPath: "./"
+function setMode(env) {
+  if (env === "development") {
+    return {
+      mode: "development",
+      entry: "./src/index",
+      output: {
+        path: resolve(__dirname, "build"),
+        filename: "static/js/[name].js",
+        chunkFilename: "static/js/[name].chunk.js",
+        publicPath: "/"
+      }
+    };
   }
-};
+  return {
+    mode: "production",
+    entry: "./src/index",
+    output: {
+      path: resolve(__dirname, "build"),
+      filename: "static/js/[name].[chunkhash:8].js",
+      chunkFilename: "static/js/[name].[chunkhash:8].chunk.js",
+      publicPath: "./"
+    }
+  };
+}
+
+module.exports = setMode;
